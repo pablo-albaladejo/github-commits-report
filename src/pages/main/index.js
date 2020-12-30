d39553c43aa4d3aa92f55d785e47d87afd9c0af0
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-12-30 21:22:27 +0100

    chore(master): CI/CD basename

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import {
-  BrowserRouter, Switch, Route,
-  //Redirect 
-} from 'react-router-dom';
+  BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import LoginPage from '../login';
-//import HomePage from '../home';
+import HomePage from '../home';
 import ErrorPage from '../error';
-//import ProtectedRoute from '../../router';
+import ProtectedRoute from '../../router';
 
 const Home = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      basename={process.env.REACT_APP_BASENAME}
+    >
       <Switch>
-        <Route path='/' component={LoginPage} />
-        <Route path='/error' component={ErrorPage} />
-        {/*<ProtectedRoute exact path='/' component={HomePage} />
+        <ProtectedRoute exact path='/' component={HomePage} />
         <Route path='/login' component={LoginPage} />
         <Route path='/error' component={ErrorPage} />
-        <Redirect to="/error" /> */}
+        <Redirect to="/error" />
       </Switch>
     </BrowserRouter>
   )
