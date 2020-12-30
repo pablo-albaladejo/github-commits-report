beca5fc924e653cc5f2871868ac873ede1bd4a17
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-12-30 16:08:39 +0100

    chore(master): homepage base url

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import LoginPage from '../login';
 import HomePage from '../home';
 import ErrorPage from '../error';
@@ -7,12 +7,14 @@ import ProtectedRoute from '../../router';
 
 const Home = () => (
   <BrowserRouter>
-    <Switch>
-      <ProtectedRoute exact path='/' component={HomePage} />
-      <Route exact path='/login' component={LoginPage} />
-      <Route exact path='/error' component={ErrorPage} />
-      <Redirect to="/error" />
-    </Switch>
+    <HashRouter>
+      <Switch>
+        <ProtectedRoute exact path='/' component={HomePage} />
+        <Route exact path='/login' component={LoginPage} />
+        <Route exact path='/error' component={ErrorPage} />
+        <Redirect to="/error" />
+      </Switch>
+    </HashRouter>
   </BrowserRouter>
 )
 export default Home;
