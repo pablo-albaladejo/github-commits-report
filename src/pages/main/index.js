eee1aa6562b2781cfe86525d490c143086e4badd
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-12-30 19:19:44 +0100

    chore(master): CI/CD test

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route,
+  //Redirect 
+} from 'react-router-dom';
 import LoginPage from '../login';
-import HomePage from '../home';
-import ErrorPage from '../error';
-import ProtectedRoute from '../../router';
+//import HomePage from '../home';
+//import ErrorPage from '../error';
+//import ProtectedRoute from '../../router';
 
 const Home = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <ProtectedRoute exact path='/' component={HomePage} />
+        <Route path='/' component={LoginPage} />
+        {/*<ProtectedRoute exact path='/' component={HomePage} />
         <Route path='/login' component={LoginPage} />
         <Route path='/error' component={ErrorPage} />
-        <Redirect to="/error" />
+        <Redirect to="/error" /> */}
       </Switch>
     </BrowserRouter>
   )
