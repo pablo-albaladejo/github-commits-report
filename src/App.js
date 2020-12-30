ec0874b1cbcac3af0d1b502c041796b926473eee
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-12-30 16:45:36 +0100

    chore(master): CI/CD test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { AuthContext } from './context';
+import Home from './pages/home';
 
-import Main from './pages/main';
+//import Main from './pages/main';
 import { initialState, reducer } from "./store/reducer";
 
 const App = () => {
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
   return (
-    <AuthContext.Provider
+/*     <AuthContext.Provider
       value={{
         state,
         dispatch
       }}
     >
       <Main />
-    </AuthContext.Provider>
+    </AuthContext.Provider> */
+    <Home/>
   )
 };
 export default App;
